70375c61ad1f0778c3248f20d2dea8eca1c2470b
abhijitmoe <146827748+abhijitmoe@users.noreply.github.com>
2025-05-20 14:11:24 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,6 @@
 /*! For license information please see serviceworker.js.LICENSE.txt */
-importScripts("https://cdn.moengage.com/release/dc_1/serviceworker_cdn.min.latest.js");
+//importScripts("https://cdn.moengage.com/release/dc_1/serviceworker_cdn.min.latest.js");
+
+importScripts(
+    `https://cdn.moengage.com/webpush/releases/serviceworker.js`
+  )
